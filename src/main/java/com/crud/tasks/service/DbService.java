1610659a6298ab34b8bf2f576293fa8e6a1825ae
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-11-16 11:51:39 +0100

    15.3 - Używanie ResponseEntity

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +18,11 @@ public class DbService {
     return repository.findAll();
   }
 
-  public Optional<Task> getTaskById(Long taskId) {
-    return repository.findById(taskId);
+  public Task getTaskById(Long taskId) throws TaskNotFoundException {
+    return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
+  }
+
+  public Task saveTask(final Task task) {
+    return repository.save(task);
   }
 }
