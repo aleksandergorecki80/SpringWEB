1923ad2da811e3ed62ece83884ef30a7a0a4ec73
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-11-19 17:50:26 +0100

    16.4 - Wysylanie danych za pomoca ajax i wyswietlanie ich na widoku

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/tasks/")
+@RequestMapping("/v1/tasks")
 public class TaskController {
 
   private final DbService service;
