02178afe51c3a4a45039b9a73fb0d7debb6e835b
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-11-19 17:51:17 +0100

    16.4 - Wysylanie danych za pomoca ajax i wyswietlanie ich na widoku (#6)

--- src/main/java/com/crud/tasks/controller/GlobalHttpErrorHandler.java
+++ src/main/java/com/crud/tasks/controller/GlobalHttpErrorHandler.java
@@ -1,11 +1,9 @@
 package com.crud.tasks.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
