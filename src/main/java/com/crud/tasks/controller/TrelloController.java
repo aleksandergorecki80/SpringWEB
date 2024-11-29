dea3a24ee052ca1679a36df54474c00c2ef1504b
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-11-29 20:23:54 +0100

    18.3. Rozbudowa aktualnej metody getTrelloBoards

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @RestController
 @RequestMapping("v1/trello")
 @RequiredArgsConstructor
@@ -20,17 +19,14 @@ public class TrelloController {
 
   @GetMapping("boards")
   public void getTrelloBoards() {
-
     List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-    trelloBoards
-        .stream()
-        .filter(trelloBoardDto -> trelloBoardDto.getName() != null)
-        .filter(trelloBoardDto -> trelloBoardDto.getId() != null)
-        .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
-        .forEach(trelloBoardDto -> {
-          System.out.println(trelloBoardDto);
-      System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+    trelloBoards.forEach(trelloBoardDto -> {
+      System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+      System.out.println("This board contains lists: ");
+      trelloBoardDto.getLists().forEach(trelloList -> {
+        System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+      });
     });
   }
 }
