e8ca3bfc4e21369291f228652a9c645797249ce2
aleksandergorecki80 <aleksandergorecki80@gmail.com>
2024-11-30 13:32:36 +0100

    18.3. Rozbudowa klienta

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,12 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -28,5 +27,10 @@ public class TrelloController {
         System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
       });
     });
+  }
+
+  @PostMapping("cards")
+  public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+    return trelloClient.createNewCard(trelloCardDto);
   }
 }
