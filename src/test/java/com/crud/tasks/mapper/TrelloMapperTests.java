2ea12b68693f16595e6039e4e29e4b792bebcdc8
Aleksander <aleksandergorecki80@gmail.com>
2025-01-10 22:34:47 +0100

    21.1. Stosowanie wzorca fasady w projekcie

--- src/test/java/com/crud/tasks/mapper/TrelloMapperTests.java
+++ src/test/java/com/crud/tasks/mapper/TrelloMapperTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TrelloMapperTests {
     private final TrelloMapper trelloMapper = new TrelloMapper();
@@ -82,8 +82,81 @@ public class TrelloMapperTests {
         // Then
         assertEquals(2, result.size());
 
-        assertEquals("1", trelloBoardsDto.getFirst().getId());
-        assertEquals("Board 2", trelloBoardsDto.getLast().getName());
+        assertEquals("1", result.getFirst().getId());
+        assertEquals("Board 2", result.getLast().getName());
+    }
 
+    @Test
+    void testMapToCardDto() {
+        // Given
+        TrelloCard card = new TrelloCard("Task 1", "Description of the task", "top", "12345");
+
+        // When
+        TrelloCardDto result = trelloMapper.mapToCardDto(card);
+
+        // Then
+        assertEquals("Task 1", result.getName());
+        assertEquals("Description of the task", result.getDescription());
+        assertEquals("top", result.getPos());
+        assertEquals("12345", result.getListId());
+    }
+
+    @Test
+    void testMapToListDto() {
+        // Given
+        TrelloList list1 = new TrelloList("1", "To Do", false);
+        TrelloList list2 = new TrelloList("2", "Done", true);
+
+        List<TrelloList> trelloListList= new ArrayList<>();
+        trelloListList.add(list1);
+        trelloListList.add(list2);
+
+        // When
+        List<TrelloListDto> result = trelloMapper.mapToListDto(trelloListList);
+
+        // Then
+        assertEquals(2, result.size());
+
+        assertEquals("1", result.getFirst().getId());
+        assertEquals("To Do", result.getFirst().getName());
+        assertFalse(result.getFirst().isClosed());
+
+        assertEquals("2", result.getLast().getId());
+        assertEquals("Done", result.getLast().getName());
+        assertTrue(result.getLast().isClosed());
+    }
+
+    @Test
+    void testMapToBoardsDto() {
+        // Given
+        TrelloList list1 = new TrelloList("1", "To Do", false);
+        TrelloList list2 = new TrelloList("2", "Done", true);
+        TrelloList list3 = new TrelloList("3", "To Do 2", false);
+        TrelloList list4 = new TrelloList("4", "Done 2", true);
+
+        List<TrelloList> trelloListList1= new ArrayList<>();
+        trelloListList1.add(list1);
+        trelloListList1.add(list2);
+
+        List<TrelloList> trelloListList2= new ArrayList<>();
+        trelloListList2.add(list3);
+        trelloListList2.add(list4);
+
+        TrelloBoard trelloBoard1 = new TrelloBoard("1", "Board 1", trelloListList1);
+        TrelloBoard trelloBoard2 = new TrelloBoard("2", "Board 2", trelloListList2);
+
+        List<TrelloBoard> trelloBoards = new ArrayList<>();
+        trelloBoards.add(trelloBoard1);
+        trelloBoards.add(trelloBoard2);
+
+
+        // When
+        List<TrelloBoardDto> result = trelloMapper.mapToBoardsDto(trelloBoards);
+
+        // Then
+        assertEquals(2, result.size());
+
+        assertEquals("1", result.getFirst().getId());
+        assertEquals("Board 2", result.getLast().getName());
     }
 }
